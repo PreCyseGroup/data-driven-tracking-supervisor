2b9831d3cd058f86004f8858920cd276e6ff0a6d
Mehran Attar <mehran.attar@concordia.ca>
2024-09-11 21:21:47 -0400

    Update TS_Init.m

--- TS_Init.m
+++ TS_Init.m
@@ -1,9 +1,16 @@
 function R1 = TS_Init(x,u,AB,W)
 
+% Author:       Mehran Attar - Montreal, Canada
+% Written:      22-August-2024
+% Last update:  --------------
+% Last revision: ------------- 
+%---------------------------------------------------------------
+% This function initialize the tracking supervisor module.  
 
+%------------- BEGIN CODE --------------
 x1 = zonotope(x,0*diag(ones(2,1)));
 u1 = zonotope(u,0*diag(ones(2,1)));
 R1 = (AB * (cartProd(x1,u1))) + W;
-
+%------------- END CODE ----------------
 end
 
