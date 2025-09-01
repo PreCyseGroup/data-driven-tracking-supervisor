b0c9fd104ab0f4159a3a44b2b0eea16dfe3780c1
Mehran Attar <mehran.attar@concordia.ca>
2025-09-01 10:13:59 -0400

    Update sysIDtest.m

--- system_identification/sysIDtest.m
+++ system_identification/sysIDtest.m
@@ -1,5 +1,5 @@
 % Author:       Mehran Attar - Montreal, Canada
-% Written:      01-May-2025
+% Written:      15-Aug-2025
 % Last update:  --------------
 % Last revision: -------------
 %---------------------------------------------------------------
